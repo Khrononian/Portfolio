a7046a4b0ed6daf95438f1bc7d28541630153763
Alvin <Alv.Barnes@gmail.com>
2025-10-14 14:48:40 -0400

    Added link button

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Projects.css'
-
-import fallout from '../images/previewPowerOn.png'
-import steam from '../images/SteamClone.png'
+import fallout from '../images/fallouterminal.png'
+import steam from '../images/steamclone.png'
 import calculator from '../images/tscalculator.png'
-import waldo from '../images/WheresWaldo.png'
+import waldo from '../images/whereswaldo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
   return (
@@ -17,6 +18,7 @@ const Projects = () => {
                 <h3>Fallout Terminal</h3>
                 <p><span>React</span></p>
                 <p>A recreation of the in-game computer terminals from the Fallout series. Text based hacking mini game simulated with a retro futuristic green monochrome interface built with react.</p>
+                <a href='https://falloutterminalapp.netlify.app/' target='_blank'>Live Site <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
               </div>
               
               
@@ -27,6 +29,7 @@ const Projects = () => {
                 <h3>Steam Clone</h3> 
                 <p><span>React</span><span>Firebase</span><span>Material UI</span></p>
                 <p>Steam inspired web app which showcases a game library, store and user profiles. Designed with responsive UI to mimic the look and feel of a digital game platform.</p>
+                <a href='https://steampoweredclone.netlify.app/' target='_blank'>Live Site <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
               </div>
               
               
@@ -37,6 +40,7 @@ const Projects = () => {
                 <h3>TypeScript Calculator</h3>
                 <p><span>React</span><span>TypeScript</span></p>
                 <p>Simple calculator to operate any functional operations. First project to work with while learning TypeScript.</p>
+                <a href='https://tscalculate.netlify.app/' target='_blank'>Live Site <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
               </div>
               
               
@@ -47,6 +51,7 @@ const Projects = () => {
                 <h3>Where's Waldo</h3>
                 <p><span>JavaScript</span></p>
                 <p>A simple interactive app where users search for hidden characters within images.</p>
+                <a href='https://wheresthatcharacter.netlify.app/' target='_blank'>Live Site <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
               </div>
               
               
