ef48495707baa5f9b2285d9a8cfc602b18c9bfd6
Alvin <Alv.Barnes@gmail.com>
2025-11-07 19:27:46 -0500

    Added minor new things like anchor links, id names, placements

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,62 @@ import Projects from './Components/Projects'
 import Footer from './Components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareGithub, faSquareLinkedin, faHtml5, faCss3Alt, faJs, faReact, faGitAlt } from '@fortawesome/free-brands-svg-icons'
-import { faGears, faFire, faFilePdf, faEnvelope, faCode, faGlobe, faGamepad, faWifi, faDesktop, faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { faGears, faFire, faFilePdf, faEnvelope, faCode, faGlobe, faGamepad, faWifi, faDesktop, faBars, faX } from '@fortawesome/free-solid-svg-icons'
+// import { HashLink } from 'react-router-hash-link'
 import './styles/App.css'
 
 function App() {
   const [showHeaders, setShowHeaders] = useState(false) 
+  const [index, setIndex] = useState(0)
   const [chosenIcons, setChosenIcons] = useState('')
+  const [menuToggle, setMenuToggle] = useState(false)
+  const [toggleAnimation, setToggleAnimation] = useState(false)
   const icons = [faCode, faGlobe, faGamepad, faWifi, faDesktop]
   const menuRef = useRef()
+  const iconRef = useRef()
   
-  setTimeout(() => {
-    setShowHeaders(true)
-  }, 8000)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setShowHeaders(true)
+    }, 7500)
+  })
 
   useEffect(() => {
-    let current = 0;
     const interval = setInterval(() => {
-      current = (current + 1) % icons.length
-      setChosenIcons(icons[current])
+      setIndex(prev => (prev + 1) % icons.length)
+      setChosenIcons(icons[index])
     }, 7000)
 
     return () => clearInterval(interval)
-  }, [])
+  }, [index])
+
+  useEffect(() => {
+    setToggleAnimation(false)
+
+    requestAnimationFrame(() => setToggleAnimation(true))
+  }, [index, toggleAnimation])
+
+  // useEffect(() => {
+  //   const icon = iconRef.current
+  //   let current = 0;
+  //   const interval = setInterval(() => {
+  //     console.log('ICON', icon, icon.querySelector('svg'))
+
+  //     // setToggleAnimation(current => !current)
+  //     setToggleAnimation(false)
+
+  //     requestAnimationFrame(() => setToggleAnimation(true))
+    
+        
+  //     current = (current + 1) % icons.length
+  //     setChosenIcons(icons[current])
+
+      
+  //   }, 7000)
+
+  //   return () => clearInterval(interval)
+  // }, [])
 
   // const startSvgAnimation = () => {
   //   console.log('TIMER', showAnimation)
@@ -41,12 +74,15 @@ function App() {
   //     }, 5400)
   //   }, 5000)
   // }
-  const showHamburgerMenu = () => {
+  const showHamburgerMenu = (event) => {
     const menu = menuRef.current
-    console.log(menuRef, menuRef.current)
+    console.log(menuRef, menuRef.current, event.target.parentElement)
     
+    setMenuToggle(current => !current)
+
     menu.classList.toggle('hamburger-menu')
   }
+  
   const fadeUp = () => {
     const fadeUp = document.querySelectorAll('.fadeUp');
 
@@ -62,12 +98,6 @@ function App() {
   }
   window.addEventListener('scroll', fadeUp)
 
-  // while (showHeaders == true) {
-  //   setTimeout(() => {
-  //     setShowAnimation((current => !current))
-  //   }, 4000)
-  // }
-
   return (
     <main>
       <Loading />
@@ -81,17 +111,20 @@ function App() {
             <li><a href='' target='_blank'>Resume</a></li>
           </ul>
         </div> */}
-        <a onClick={showHamburgerMenu} className='menu'><FontAwesomeIcon icon={faBars}/> </a>
+        <a onClick={showHamburgerMenu} className={menuToggle == false ? 'menu slides-right' : 'menu slides-left'}><FontAwesomeIcon icon={menuToggle == false ? faBars : faX}/> </a>
         <ul className='main-list'>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-          <li><a href='' target='_blank'>Resume</a></li>
+          {/* <li><HashLink smooth to='/#about'>About</HashLink></li>
+          <li><HashLink smooth to='/#projects'>Projects</HashLink></li>
+          <li><HashLink smooth to='/#contact'>Contact</HashLink></li> */}
+          <li><a href='#about' className='anchor-links'>About</a></li>
+          <li><a href='#projects' className='anchor-links'>Projects</a></li>
+          <li><a href='#contact' className='anchor-links'>Contact</a></li>
+          <li><a href='mailto:Alv.Barnes@gmail.com' target='_blank' className='nav-resume'>Resume</a></li>
         </ul>
         
       </nav>
-      
-      <div className='menu-list' ref={menuRef}>
+
+      <div className={menuToggle == false ? 'nav-list menu-appear-up' : 'nav-list menu-appear-down'} ref={menuRef}>
         <ul>
           <li>About</li>
           <li>Projects</li>
@@ -102,10 +135,11 @@ function App() {
       {/* <header className='fadeUp intro hide'> */}
       <header className={showHeaders == false ? 'hide' : 'fadeUp active'}>
         <h1>Alvin <span>Barnes</span></h1>
-        <div className='header-name'>
+        <div className='header-name' ref={iconRef}>
           <h2>Frontend Developer</h2>
           {/* <FontAwesomeIcon icon={faCode} className={showAnimation == false ? '' : 'header-svg-animation'} /> */}
-          <FontAwesomeIcon icon={chosenIcons} className='header-svg-animation' />
+          <FontAwesomeIcon icon={chosenIcons} className={toggleAnimation == false ? 'header-svg-glow' : 'header-svg-animation'} />
+          {/* <FontAwesomeIcon icon={chosenIcons} className='header-svg-animation' /> */}
         </div>
         <div>
           <h4>Skills</h4>
@@ -122,17 +156,17 @@ function App() {
         <div className='socials-list'>
           <ul>
             <li><a href='https://github.com/Khrononian' target='_blank'><FontAwesomeIcon icon={faSquareGithub} /></a></li>
-            <li><a href='' target='_blank'><FontAwesomeIcon icon={faSquareLinkedin} /></a> </li>
-            <li><a href='' className='last-link'><FontAwesomeIcon icon={faEnvelope} /></a></li>
+            <li><a href='www.linkedin.com/in/alvin-barnes' target='_blank'><FontAwesomeIcon icon={faSquareLinkedin} /></a> </li>
+            <li><a href='mailto:Alv.Barnes@gmail.com' className='last-link'><FontAwesomeIcon icon={faEnvelope} /></a></li>
           </ul>
         </div>
       </header>
       <About />
       <Projects />
       <Footer 
-      github={faSquareGithub} 
-      linkedin={faSquareLinkedin} 
-      email={faEnvelope}
+        github={faSquareGithub} 
+        linkedin={faSquareLinkedin} 
+        email={faEnvelope}
       />
     </main>
   )
